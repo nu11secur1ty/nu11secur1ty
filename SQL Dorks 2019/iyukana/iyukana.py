a78ca2410a9bee5ade3286fd656e306b0cb963c1
nu11secur1ty <venvaropt@gmail.com>
2019-08-02 16:58:00 +0300

    Create iyukana.py

---
+++ SQL Dorks 2019/iyukana/iyukana.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Author nu11secur1ty
+
+from colorama import Fore, Back, Style 
+
+try: 
+    from googlesearch import search 
+except ImportError:  
+    print("No module named 'google' found") 
+  
+# to search 
+print(Fore.GREEN + 'Put your d0rk here\n')
+print(Style.RESET_ALL)
+
+# Do not make a lot of requests searching! ;)
+query = input()
+  
+for maaluk in search(query, tld="com", num=10, stop=25, pause=2): 
+    print(maaluk)
