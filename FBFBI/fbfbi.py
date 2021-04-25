e15109462f80bb2aae5ad53a8b5e977c644fa5aa
nu11secur1ty <venvaropt@gmail.com>
2021-04-25 10:03:54 +0300

    Update fbfbi.py

--- FBFBI/fbfbi.py
+++ FBFBI/fbfbi.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from modules import ping
 
 #enter the link to the website you want to automate login.
 website_link="https://lookup-id.com/"
